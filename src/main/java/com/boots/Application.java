a8e2f105b79c8c6af76a326c562faaa21be87a85
Kreout <ia@krot.name>
2021-05-10 23:46:29 +0300

    Update Application.java

--- src/main/java/com/boots/Application.java
+++ src/main/java/com/boots/Application.java
@@ -3,6 +3,7 @@ package com.boots;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
