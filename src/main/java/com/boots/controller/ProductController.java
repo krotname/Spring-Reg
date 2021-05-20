0d1638acf72124188caee761fdf12a64b55631d7
krotname <ia@krot.name>
2021-05-20 14:37:54 +0300

    + GraphqlClient

--- src/main/java/com/boots/controller/ProductController.java
+++ src/main/java/com/boots/controller/ProductController.java
@@ -4,10 +4,18 @@ import com.boots.entity.User;
 import com.boots.service.UserService;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
+import org.mountcloud.graphql.GraphqlClient;
+import org.mountcloud.graphql.request.query.DefaultGraphqlQuery;
+import org.mountcloud.graphql.request.query.GraphqlQuery;
+import org.mountcloud.graphql.response.GraphqlResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 public class ProductController {
@@ -17,8 +25,26 @@ public class ProductController {
 
     @GetMapping("/product")
     public String registration(Model model) {
-        model.addAttribute("userForm", new User());
-        logger.info("product" + model.toString()) ;
+
+
+        GraphqlClient client = GraphqlClient.buildGraphqlClient("http://localhost:8080/v1/graphql");
+
+        GraphqlQuery query = new DefaultGraphqlQuery("employee");
+        query.addResultAttributes("lastname", "name");
+        Map data = null;
+        try {
+            GraphqlResponse response = client.doQuery(query);
+            data = response.getData();
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        model.addAttribute("product", data);
+
+        System.out.println(data);
+        logger.info("product" + data);
         return "product";
 
     }
